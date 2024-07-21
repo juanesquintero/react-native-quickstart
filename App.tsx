07ef40145bd8f1d31185fb02be764629648a9654
Juan Quintero <juanestquintero@gmail.com>
2024-07-20 20:56:15 -0500

    feat: native button example

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
+import icon from "./assets/icon.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Quick Start</Text>
       <StatusBar style="auto" />
+      <Image
+        source={icon}
+        style={{ width: 100, height: 100 }}
+        alt="I'm an Image"
+      ></Image>
+      <Text>Quick Start</Text>
+
+      <Button
+        title="Native button"
+        color="red"
+        onPress={() => {
+          alert("Native alert Notification");
+        }}
+      />
     </View>
   );
 }
