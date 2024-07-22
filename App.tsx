166e98392f83989861993b4e07d76e406488ce5a
Juan Quintero <juanestquintero@gmail.com>
2024-07-21 20:54:00 -0500

    feat: touchable element with long press

--- App.tsx
+++ App.tsx
@@ -13,20 +13,34 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+
       <Image
         source={icon}
         style={{ width: 100, height: 100 }}
         alt="I'm an Image"
       ></Image>
-      <Text>Quick Start</Text>
 
-      <Button
-        title="Native button"
-        color="red"
-        onPress={() => {
-          alert("Native alert Notification");
+      <Text style={{ padding: 15 }}>Quick Start</Text>
+
+      <View style={{ margin: 30 }}>
+        <Button
+          title="Press me!"
+          color="red"
+          onPress={() => {
+            alert("I'm a native button");
+          }}
+        />
+      </View>
+
+      <TouchableHighlight
+        style={{ padding: 10, borderWidth: 1.5, borderRadius: 100 }}
+        underlayColor={"#09f"}
+        onLongPress={() => {
+          alert("I'm a Touchable");
         }}
-      />
+      >
+        <Text>Long press me!</Text>
+      </TouchableHighlight>
     </View>
   );
 }
